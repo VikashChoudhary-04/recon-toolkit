db61a265b2aafaa2265bc862f655ef90bc96a7be
Vikash Choudhary <120484001+VikashChoudhary-04@users.noreply.github.com>
2026-03-01 11:48:09 +0530

    Update passive.sh

--- modules/passive.sh
+++ modules/passive.sh
@@ -3,12 +3,11 @@
 domain=$1
 
 echo "[*] Running Sublist3r..."
-sublist3r -d $domain -o sublist3r.txt
-
+subfinder -d $domain -silent -o subfinder.txt
 echo "[*] Running GAU..."
 gau $domain > gau.txt
 
 echo "[*] Running Chaos..."
 chaos -d $domain -o chaos.txt
 
-cat sublist3r.txt chaos.txt | sort -u > subdomains.txt
+cat subfinder.txt chaos.txt | sort -u > subdomains.txt
