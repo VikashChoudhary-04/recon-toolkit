a5449806179fcd3d63b00ddd84b514b4df05d171
Vikash Choudhary <120484001+VikashChoudhary-04@users.noreply.github.com>
2026-03-01 11:13:37 +0530

    Create full-recon.sh

---
+++ workflows/full-recon.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+domain=$1
+
+if [ -z "$domain" ]; then
+  echo "Usage: ./full-recon.sh example.com"
+  exit 1
+fi
+
+mkdir -p ../output/$domain
+cd ../output/$domain || exit
+
+echo "[*] Starting Full Recon for $domain"
+
+bash ../../modules/passive.sh $domain
+bash ../../modules/dns-validation.sh
+bash ../../modules/crawling.sh
+bash ../../modules/js-analysis.sh
+bash ../../modules/fuzzing.sh
+bash ../../modules/nuclei-scan.sh
+bash ../../modules/dalfox-scan.sh
+
+echo "[+] Recon Completed for $domain"
