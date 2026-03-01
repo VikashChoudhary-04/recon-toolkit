00f7e36f67ba3ecdebd9b4e92b66385e27263e2d
Vikash Choudhary <120484001+VikashChoudhary-04@users.noreply.github.com>
2026-03-01 11:15:26 +0530

    Delete modules/modules directory

--- modules/modules/dns-validation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "[*] Validating Subdomains..."
-dnsx -l subdomains.txt -o dnsx.txt
-
-echo "[*] Checking Live Hosts..."
-httpx -l dnsx.txt -silent -title -status-code -o live.txt
